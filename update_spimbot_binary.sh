76f6f4547e72b0a7f933a733ebf1b292b4a39f6d
Eric Zhang <ezhang887@gmail.com>
2022-02-12 00:07:36 -0600

    Add update script

---
+++ update_spimbot_binary.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+ARGC=$#
+REPO_DIR=$1
+BINARY_VERSION=$2
+
+DEST_PATH=~/.local/bin/QtSpimbot
+
+if [ $ARGC -ne 2 ]; then
+    echo "Invalid usage, exiting..."
+    echo "Usage: ./update_spimbot_binary.sh <path to spimbot_binaries repo> <binary version>"
+    echo "Example: ./update_spimbot_binary.sh ~/.spimbot/spimbot_binares linux_x86_64"
+    exit 1
+fi;
+
+if [ ! -d $REPO_DIR ]; then
+    echo "REPO_DIR is not a valid directory, exiting..."
+    exit 2
+fi;
+
+if [ ! -d $REPO_DIR/$BINARY_VERSION ]; then
+    echo "BINARY_VERSION is not a directory in REPO_DIR, exiting..."
+    exit 3
+fi;
+
+pushd $REPO_DIR > /dev/null
+
+commitdiff=$(git rev-list HEAD...origin/main --count)
+
+if [ $commitdiff -gt 0 ]; then
+    echo "New remote commit detected, pulling + installing..."
+    git checkout main && git pull
+    cp $BINARY_VERSION/QtSpimbot $DEST_PATH
+fi;
+
+popd > /dev/null
